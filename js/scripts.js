3f3c8d1430aa345f3aed1d978dd4ad2ce1989eb5
Kiernan2 <kiernan1994@gmail.com>
2021-08-09 15:33:10 -0700

    Added UI logic and now supports name input

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,36 @@
 // Business logic
-function numberConverter(number) {
+function numberConverter(number, name) {
   if (number.toString().includes("3")) {
-    return "Won't you be my neighbor?";
+    return name + " Won't you be my neighbor? ";
   } else if (number.toString().includes("2")) {
-    return "Boop!";
+    return "Boop! ";
   } else if (number.toString().includes("1")) {
-    return "Beep!";
+    return "Beep! ";
   } else {
-    return number;
+    return number + " ";
   }
 }
 
-function arrayMaker(number) {
+function arrayMaker(number, name) {
   let array = [];
   let newArray = [];
   for (let i = 0;i <= parseInt(number);i++) {
     array.push(i.toString());
   }
   array.forEach(function(number) {
-    newArray[number] = numberConverter(number);
+    newArray[number] = numberConverter(number, name);
   })
   return newArray
 }
 
+console.log(numberConverter(3, "bob"))
 // UI logic
 
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    event.preventDefault();
+    let input = ($("input#input").val());
+    let input2 = ($("input#inputTwo").val());
+    $("#output").html(arrayMaker(input, input2));
+  })
+})
